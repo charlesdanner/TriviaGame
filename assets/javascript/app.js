cac3ab204d415cd9aaa60c519db10ea8a48c7340
Charles Danner <danner.ced1@gmail.com>
2019-08-12 14:42:35 -0400

    <when i press start quiz the button disappears and the quiz appears.>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 var correct = 0;
 var incorrect = 0;
 var unanswered = 6;
+var startQuiz = document.getElementById("start-game");
 
 document.getElementById("start").addEventListener("click", function(){
-    var startQuiz = document.getElementById("start-game");
     startQuiz.style.display = "none";
+    document.getElementById("quiz").style.display = "block"
+
+
 })
 
 
