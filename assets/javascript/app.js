d9d3afbd28ee33c6dac9c90a1cb61a530fcbb6bf
Charles Danner <danner.ced1@gmail.com>
2019-08-12 13:47:06 -0400

    <start button disappears when you hit the start button.>

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    
+ 
+var correct = 0;
+var incorrect = 0;
+var unanswered = 6;
+
+document.getElementById("start").addEventListener("click", function(){
+    var startQuiz = document.getElementById("start-game");
+    startQuiz.style.display = "none";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
